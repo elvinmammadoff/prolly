39b1594e405acbb4696b064ffbc91d326a0901be
elvinmammadoff <elvinmammadoff@gmail.com>
2018-09-09 01:24:59 +0400

    added dropdown to thw mobile version

--- gulpfile.js
+++ gulpfile.js
@@ -60,6 +60,7 @@ gulp.task('styles', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/bootstrap/dist/js/bootstrap.min.js',
 		'app/libs/owl.carousel/dist/owl.carousel.min.js',
 		'app/libs/isotope-layout/dist/isotope.pkgd.min.js',
 		'app/libs/imagesloaded/imagesloaded.pkgd.min.js',
